4cbe72a4df8c32221310cbf328161573d95e5473
Soomali <mehmetaliguunaydin@gmail.com>
2023-05-07 01:32:35 +0300

    clean-up.

--- example/job_architecture_example.dart
+++ example/job_architecture_example.dart
@@ -1,6 +1,3 @@
 import 'package:job_architecture/job_architecture.dart';
 
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
+void main() {}
--- lib/job_architecture.dart
+++ lib/job_architecture.dart
@@ -1,9 +1,4 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library job_architecture;
 
-export 'src/job_architecture_base.dart';
 export 'src/job_response/main.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+export 'src/job/main.dart';
