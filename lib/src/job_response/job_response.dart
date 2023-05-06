d36c970be0ae920277980cf03195e5312a954fd7
Soomali <mehmetaliguunaydin@gmail.com>
2023-05-07 01:43:10 +0300

    fixed job response.

--- lib/src/job_response/job_response.dart
+++ lib/src/job_response/job_response.dart
@@ -2,8 +2,16 @@ part of 'main.dart';
 
 class JobResponse<T> {
   final T? result;
+  final String? id;
   final String? error;
   final JobResponseStatus status;
   const JobResponse(
-      {this.result, this.error, this.status = JobResponseStatus.ongoing});
+      {this.id,
+      this.result,
+      this.error,
+      this.status = JobResponseStatus.ongoing});
+  @override
+  bool operator ==(Object? other) {
+    return other is JobResponse<T> && other.id == id;
+  }
 }
