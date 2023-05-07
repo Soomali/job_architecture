42cdc852af52a58ed7ecf07b600ae2a558b06b1d
Soomali <mehmetaliguunaydin@gmail.com>
2023-05-07 03:09:44 +0300

    exported job completer.

--- lib/job_architecture.dart
+++ lib/job_architecture.dart
@@ -2,4 +2,6 @@ library job_architecture;
 
 export 'src/job_response/main.dart';
 export 'src/job/main.dart';
+export 'src/chained_job/main.dart';
+export 'src/job_completer/main.dart';
 export 'src/job_handler/main.dart';
